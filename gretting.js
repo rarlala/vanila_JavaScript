c1149fec7bc2d3e9b7f9dcd6be66bc12896274af
JEONGSUJI <48057629+JEONGSUJI@users.noreply.github.com>
2019-05-30 23:52:45 +0900

    오늘의 작업
    시간이 1초 단위 업데이트, 이름 값 받아서 저장해보여주기

---
+++ gretting.js
@@ -0,0 +1,44 @@
+const form = document.querySelector(".js-form"),
+    input = form.querySelector("input"),
+    greeting = document.querySelector(".js-greetings");
+
+const USER_LS ="currentUser",
+    SHOWING_ON = "showing";
+
+function saveName(text){
+    localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(){
+    event.preventDefault();
+    const currentValue = input.value;
+    //console.log(currentValue);
+    paintGreeting(currentValue);
+}
+
+function askForName(){
+    form.classList.add(SHOWING_ON);
+    form.addEventListener("submit",handleSubmit);
+}
+
+function paintGreeting(text){
+    form.classList.remove(SHOWING_ON);
+    greeting.classList.add(SHOWING_ON);
+    greeting.innerText = `Hello ${text}`
+    saveName(currentValue);
+}
+
+function loadName(){
+    const currentUser= localStorage.getItem(USER_LS);
+    if(currentUser === null){
+        askForName();
+    }else{
+        paintGreeting(currentUser);
+    }
+}
+
+function init(){
+    loadName();
+}
+
+init();
